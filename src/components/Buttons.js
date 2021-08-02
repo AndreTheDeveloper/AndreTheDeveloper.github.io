867e9864f4c171582adef3f79135b420b7dec5fa
AndreTheDeveloper <andre.colaris98@live.ca>
2021-08-02 14:44:22 -0600

    Added dynamic resolution to fit any screen

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -6,22 +6,32 @@ import {Link} from 'react-router-dom'
 class Buttons extends Component {
     render() {
         return (
-            <span>
+            <span className="outer">
+                <div className="inner">
                 <Link to='home'>
                     <button>Home</button>
                 </Link>
+                </div>
+                <div className="inner">
                 <Link to='about'>
                     <button>About us</button>
                 </Link>
+                </div>
+                <div className="inner">
                 <a href='https://discord.gg/nJaPqRq'>
                     <button>Discord</button>
                 </a>
+                </div>
+                <div className="inner">
                 <Link to='events'>
                     <button>Scheduled Events</button>
                 </Link>
+                </div>
+                <div className="inner">
                 <Link to='contact'>
                     <button>Contact</button>
                 </Link>
+                </div>
             </span>
         );
     }
